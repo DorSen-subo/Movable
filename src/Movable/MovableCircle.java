b186594bf9c77b4a49a8c8d1848beb2590f25b78
DorSen-subo <Zontdan@gmail.com>
2020-09-16 19:50:30 +0300

    Initial commit

---
+++ src/Movable/MovableCircle.java
@@ -0,0 +1,23 @@
+public class MovableCircle implements Movable {
+    private int radius;
+    private MovablePoint center;
+    public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius) {
+        this.center = new MovablePoint(x, y, xSpeed, ySpeed);
+        this.radius = radius;
+    }
+    public String toString() {
+        return "MovableCircle{radius=" + this.radius + ", center=" + this.center + "}";
+    }
+    public void moveUp() {
+        ++this.center.y;
+    }
+    public void moveDown() {
+        --this.center.y;
+    }
+    public void moveRight() {
+        ++this.center.x;
+    }
+    public void moveLeft() {
+        --this.center.x;
+    }
+}
